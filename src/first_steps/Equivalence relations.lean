9d0f2f3dc99e2fdca5c0beaae15406fb59a099bb
matematiflo <7537413+matematiflo@users.noreply.github.com>
2023-04-02 13:43:40 +0200

    Updated equiv_rel

--- src/first_steps/Equivalence relations.lean
+++ src/first_steps/Equivalence relations.lean
@@ -7,10 +7,14 @@ def even (n : ℤ) : Prop := ∃ k : ℤ, n = 2 * k
 -- #check even 2
 -- #check even 3
 
+-- #print even 
+
 example : even 2 := by {use 1, rw mul_one}
 -- lemma two_is_even : even 2 := by {use 1, simp}
 
 def even_diff (n m : ℤ) : Prop := even (n - m)
+
+-- #print even_diff 
 
 -- infix ` ≡ `:50 := even_diff
 
@@ -46,37 +50,37 @@ theorem r_is_equiv_rel : equivalence r :=
     },
     {
       unfold symmetric,
-    intros x y,
-    unfold r,
-    unfold even_diff,
-    unfold even,
-    intro h,
-    cases h with k h_k,
-    use -k,
-    simp,
-    apply eq_neg_of_eq_neg,
-    rw ← mul_neg_one,
-    simp,
-    symmetry,
-    exact h_k,
+      intros x y,
+      unfold r,
+      unfold even_diff,
+      unfold even,
+      intro h,
+      cases h with k h_k,
+      use -k,
+      simp,
+      apply eq_neg_of_eq_neg,
+      rw ← mul_neg_one,
+      simp,
+      symmetry,
+      exact h_k,
     },
     {
-       unfold transitive r even_diff even,
-    intros x y z,
-    intro h_xy,
-    cases h_xy with k h_xy,
-    intro h_yz,
-    cases h_yz with l h_yz,
-    use k+l,
-    have h : x-z= 2*k + 2*l :=
-      begin
-        have h : (x-y) + (y-z) = x-z := by simp,
-        rw ← h,
-        rw h_xy,
-        rw h_yz,
-      end,
-    rw h,
-    rw mul_add,
+      unfold transitive r even_diff even,
+      intros x y z,
+      intro h_xy,
+      cases h_xy with k h_xy,
+      intro h_yz,
+      cases h_yz with l h_yz,
+      use k+l,
+      have h : x-z= 2*k + 2*l :=
+        begin
+          have h : (x-y) + (y-z) = x-z := by simp,
+          rw ← h,
+          rw h_xy,
+          rw h_yz,
+        end,
+      rw h,
+      rw mul_add,
     },
   end
 
@@ -95,6 +99,9 @@ lemma reflexive_r : reflexive r :=
     use 0,
     simp,
   end
+
+-- #print reflexive_r 
+-- #print r_is_equiv_rel 
 
 lemma symmetric_r : symmetric r := 
   begin
@@ -143,3 +150,9 @@ theorem r_is_equiv_rel_yet_another_proof : equivalence r :=
 ⟨reflexive_r, symmetric_r, transitive_r⟩
 
 -- Note: the syntax ⟨reflexive_r, symmetric_r, transitive_r⟩ does not seem to work in tactic mode, at least not directly. 
+
+theorem r_is_equiv_rel_alt_tactic_mode : equivalence r := 
+begin
+--unfold equivalence,
+exact mk_equivalence r reflexive_r symmetric_r transitive_r,
+end
